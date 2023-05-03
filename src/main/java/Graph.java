00ea9e0bc3b71166ca441887126522a93010edb4
Jonathan-Beer-YU <jbeer2@mail.yu.edu>
2023-05-03 18:01:57 -0400

    turned node<E> into just E

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -3,26 +3,26 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Graph<E> {
-	HashSet<node<E>> nodes;
-	HashSet<edge<node<E>>> edges; 
+	HashSet<E> nodes;
+	HashSet<edge<E>> edges; 
 		
 	public Graph() {
-		this.nodes = new HashSet<node<E>>();
-		this.edges = new HashSet<edge<node<E>>>();
+		this.nodes = new HashSet<E>();
+		this.edges = new HashSet<edge<E>>();
 	}
 	
 	public void connect(E a, E b) {
-		edges.add(new edge<node<E>>(new node<E>(a), new node<E>(b)));
+		edges.add(new edge<E>(a, b));
 	}
 	
 	public void addnode(E a) {
-		nodes.add(new node<E>(a));
+		nodes.add(a);
 	}
 	
 	public HashSet<E>getAllConnectedTo(E a) {
-		node<E> node = null;
-		for(node<E> ex : nodes) {
-			if(ex.datum.equals(a)) {
+		E node = null;
+		for(E ex : nodes) {
+			if(ex.equals(a)) {
 				node = ex;
 			}
 		}
@@ -30,38 +30,14 @@ public class Graph<E> {
 			throw new NoSuchElementException();
 		}
 		HashSet<E> output = new HashSet<E>();
-		for(edge<node<E>> edge : edges) {
-			if(edge.contains(new node<E>(a))) {
-				output.add(edge.getother(node).datum);
+		for(edge<E> edge : edges) {
+			if(edge.contains(a)) {
+				output.add(edge.getother(node));
 			}
 		}
 		return output;
 	}
 	
-	//creates a complete graph K_n; TODO: update for more kinds of graphs.
-	public Graph(String str, int n) {
-		this.nodes = new HashSet<node<E>>();
-		this.edges = new HashSet<edge<node<E>>>();
-		if(str.equals("complete")|| str.equals("Complete")) {
-			for(int i = 0; i < n; i++) {
-				nodes.add(new node<E>());
-			}
-			for(node<E> nd : nodes) {
-				for(node<E> de : nodes) {
-					edges.add(new edge<node<E>>(nd,de));
-				}
-			}
-		}
-	}
-	
-	private class node<T>{
-		public T datum;
-		public node() {
-		}
-		public node(T datum) {
-			this.datum = datum;
-		}
-	}
 	
 	private class edge<T>{
 		private final HashSet<T> set;
